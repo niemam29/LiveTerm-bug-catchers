35754a398ea3d39351de05f073aea869c22c79de
MilanVojnovic95 <milan.vojnovic995@gmail.com>
2022-09-26 14:04:50 +0200

    bug catchers v1.0 WIP

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,19 @@
+import axios from 'axios';
+import config from '../../config.json';
 
+
+export const getWeather = async (city: string) => {
+  try {
+    const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getQuote = async () => {
+  const { data } = await axios.get('https://api.quotable.io/random');
+  return {
+    quote: `“${data.content}” — ${data.author}`,
+  };
+};
