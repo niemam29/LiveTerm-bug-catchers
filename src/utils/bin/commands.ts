3f3a69c66a4039eee28df90b2fa88ee0c020e4ba
MilanVojnovic95 <milan.vojnovic995@gmail.com>
2022-09-26 08:31:03 +0200

    bug catchers v1.0

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -22,35 +22,74 @@ Type 'sumfetch' to display summary.
 `;
 };
 
-// Redirection
-export const repo = async (args: string[]): Promise<string> => {
-  window.open(`${config.repo}`);
-  return 'Opening Github repository...';
-};
 
 // About
 export const about = async (args: string[]): Promise<string> => {
-  return `Hi, I am ${config.name}. 
-Welcome to my website!
-More about me:
+  return `Hi, We are ${config.name}. 
+Welcome to our website!
+More about us:
 'sumfetch' - short summary.
-'resume' - my latest resume.
-'readme' - my github readme.`;
-};
 
-export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
-  return 'Opening resume...';
-};
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+​​### Who are Bugcatchers?
+
+
+Bug catcher is a well-experienced QA who is familiar with the principles and products of WEB 3.0 of its functioning, and is also educated about access and testing of WEB 3.0 products as well as functioning in communities like DAOs.
+- The primary responsibility of the bug catcher is to prevent defects.
+In addition, they are responsible for identifying and eliminating any mistakes in code before its release.
+Such errors could result from various issues such as insufficient data input or an improperly designed interface element. 
+- Bug catcher responsibilities include designing and implementing tests, debugging, and defining corrective actions. 
+- Bug catcher will also review system requirements and track quality assurance metrics (e.g. defect densities and open defect counts.) 
+- The bug catcher role plays an important part in the product development process. Our bug catchers are responsible for conducting tests before product launches to ensure dapp runs smoothly and meets client (community) needs while being cost-effective.
+
+### Bugcatcher duties?
+
+- Review requirements, specifications, and technical design documents to provide timely and meaningful feedback
+- Create detailed, comprehensive, and well-structured test plans and test cases
+- Estimate, prioritize, plan, and coordinate testing activities
+- Identify, record, and thoroughly document and track bugs
+- Perform thorough regression testing when bugs are resolved
+- Develop and apply testing processes for new and existing products to meet client needs
+- Liaise with internal teams (e.g., developers and product managers) to identify system requirements
+- Monitor debugging process results
+- Track quality assurance metrics, like defect densities and open defect counts
+- Stay up-to-date with new testing tools and test strategies
+- Exposure to CI/CD
+
+### Bug Catchers Tech Stack:
+
+- JS, TS
+- Cypress / Synpress
+- Gherkin
+- Java
+- Selenium
+- WebDriverIO
+
+
+### What you can expect from the bug catchers squad and how we are organized:
+
+Bug Catchers QA squad is formed around certain tasks and has a certain budget for it, and certain goals per quarter.
+So that the DAO community can hold us accountable and periodically (every three months for example) hold a QA retrospective presentation,
+where the contribution of the QA squad would often be seen and the ratio of the contribution and cost of the QA team would be considered to align or change according to the needs of the projects and the impact on project development.
+I think this would make it easier for any other project leader because he would be responsible for budget calculations and goal planning only for the devs of a certain team or project and not for us from the QA team.
+
+### QA squad leader/team would have the following duties:
+
+1. Analyzing the needs of projects together with the Leader of the same and making a plan of QA actions and contributions.
+
+2. Planning the budgets/timelines for the QA squad after creating an action plan for the project.
+ 
+3. Should know all projects well enough to organize QA contributors, prioritize tasks and "step in" as help when needed.
+
+4. Creating a QA retrospective report, which would show the actions and monitor the performance of the QA team for each project individually so that the community would be aware of our contribution.
+
+5. Performing QA tasks like other QA contributors (manual testing and developing automation tests)
+
+
 `;
 };
+
+
 
 // Contact
 export const email = async (args: string[]): Promise<string> => {
@@ -58,43 +97,7 @@ export const email = async (args: string[]): Promise<string> => {
   return `Opening mailto:${config.email}...`;
 };
 
-export const github = async (args: string[]): Promise<string> => {
-  window.open(`https://github.com/${config.social.github}/`);
-
-  return 'Opening github...';
-};
-
-export const linkedin = async (args: string[]): Promise<string> => {
-  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
-
-  return 'Opening linkedin...';
-};
-
-// Search
-export const google = async (args: string[]): Promise<string> => {
-  window.open(`https://google.com/search?q=${args.join(' ')}`);
-  return `Searching google for ${args.join(' ')}...`;
-};
-
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
-
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
-};
-
-export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
-  return `Searching reddit for ${args.join(' ')}...`;
-};
-
 // Typical linux commands
-export const echo = async (args: string[]): Promise<string> => {
-  return args.join(' ');
-};
 
 export const whoami = async (args: string[]): Promise<string> => {
   return `${config.ps1_username}`;
@@ -108,10 +111,6 @@ fake
 directories`;
 };
 
-export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
-};
 
 export const date = async (args: string[]): Promise<string> => {
   return new Date().toString();
@@ -141,17 +140,19 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+Wlecome to WEB 3.0 QA Squad
+
+
+  ██████╗ ██╗   ██╗ ██████╗      ██████╗ █████╗ ████████╗ ██████╗██╗  ██╗███████╗██████╗ ███████╗
+  ██╔══██╗██║   ██║██╔════╝     ██╔════╝██╔══██╗╚══██╔══╝██╔════╝██║  ██║██╔════╝██╔══██╗██╔════╝
+  ██████╔╝██║   ██║██║  ███╗    ██║     ███████║   ██║   ██║     ███████║█████╗  ██████╔╝███████╗
+  ██╔══██╗██║   ██║██║   ██║    ██║     ██╔══██║   ██║   ██║     ██╔══██║██╔══╝  ██╔══██╗╚════██║
+  ██████╔╝╚██████╔╝╚██████╔╝    ╚██████╗██║  ██║   ██║   ╚██████╗██║  ██║███████╗██║  ██║███████║
+  ╚═════╝  ╚═════╝  ╚═════╝      ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+                                                                                                 
 
 Type 'help' to see the list of available commands.
+Type 'about' to read more about us.
 Type 'sumfetch' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -3,22 +3,6 @@ import config from '../../../config.json';
 const sumfetch = async (args: string[]): Promise<string> => {
   if (config.ascii === 'cveinnt') {
     return `                                                  
-             @@@@@@@@@@@@@                   sumfetch: summary display
-        @@@@               @@@@             -----------
-      @@                       @@            ABOUT
-    @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-@@                                   @@     -----------
-@@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-         @|                      @@          <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
 
 `;
   } else {
@@ -27,17 +11,21 @@ const sumfetch = async (args: string[]): Promise<string> => {
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓        
 ▐▓                                 ▐▓       -----------
-▐▓        > L I V E T E R M        ▐▓        CONTACT 
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+▐▓        >  BUG CATCHERS          ▐▓        CONTACT 
+▐▓        >  WEB 3.0 QA            ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.discord}" target="_blank">${config.social.discord}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓          
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             Team 
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓             * Milan                
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               <u><a>https://github.com/MilanVojnovic95</a></u>
+                                            * Phillip
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  <u><a>https://github.com/Filipv95</a></u>
+                                            * Kacper
+                                            <u><a>https://github.com/niemam29</a></u>
 
 `;
   }
