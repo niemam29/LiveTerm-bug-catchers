2959f3879d7a17e275be8a3aa1f7033a00b1c168
MilanVojnovic95 <milan.vojnovic995@gmail.com>
2022-09-26 14:05:49 +0200

    bug catchers v1.0 WIP

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,8 +1,7 @@
 // // List of commands that require API calls
 
-import { getProjects } from '../api';
+
 import { getQuote } from '../api';
-import { getReadme } from '../api';
 import { getWeather } from '../api';
 
 
